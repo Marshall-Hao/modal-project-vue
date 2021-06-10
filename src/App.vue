4b9f3f2a79a44d4624cd8bac469ef811c81ec033
marshall chen <marshallchan666@outlook.com>
2021-06-11 02:35:06 +0800

    event modify @click.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div v-if="showModal">
     <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
   </div> 
-  <button @click="toggleModal">open Modal</button>
+  <button @click.alt="toggleModal">open Modal(alt)</button>
   <!-- <input type="text" ref="name">
   <button @click="handleClick">click me</button> -->
 </template>
